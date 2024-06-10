5325d95300f12fa1932c322c097f7659cdc4894a
Kyle Robertson <me@iwolfking.xyz>
2024-06-10 14:47:54 -0500

    Fix VaultarHUD crashes

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaultarhud/MixinInventoryHudRender.java
@@ -0,0 +1,24 @@
+package xyz.iwolfking.woldsvaults.mixins.vaultarhud;
+
+import io.iridium.vaultarhud.renderers.HUDInventoryRenderer;
+import io.iridium.vaultarhud.util.Point;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.minecraft.client.gui.screens.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.iwolfking.woldsvaults.lib.vaultarhud.WoldScreenValidator;
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.MOD, value = "vaultarhud")
+        }
+)
+@Mixin(value = HUDInventoryRenderer.class, remap = false)
+public abstract class MixinInventoryHudRender {
+
+    @Redirect(method = "render", at = @At(value = "INVOKE_ASSIGN", target = "Lio/iridium/vaultarhud/util/ScreenValidator;getScreenHUDCoordinates(Lnet/minecraft/client/gui/screens/Screen;Lio/iridium/vaultarhud/util/Point;)Lio/iridium/vaultarhud/util/Point;"))
+    private static Point callWoldScreenValidator(Screen screen, Point point) {
+        return WoldScreenValidator.getScreenHUDCoordinates(screen, point);
+    }
+}
