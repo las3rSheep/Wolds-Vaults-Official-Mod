24feddc9c34fa9bc211734b89dff161a0fed75a8
Kyle Robertson <me@iwolfking.xyz>
2024-07-08 13:50:50 -0500

    Legendary jewels recycling always gives a gemstone

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/immersiveengineering/MixinEventHandler.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/immersiveengineering/MixinEventHandler.java
@@ -3,9 +3,7 @@ package xyz.iwolfking.woldsvaults.mixins.immersiveengineering;
 import blusunrize.immersiveengineering.common.EventHandler;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = EventHandler.class, remap = false)
 public class MixinEventHandler {
@@ -14,8 +12,8 @@ public class MixinEventHandler {
      * @author iwolfking
      * @reason Disable dropping shader bags in vaults
      */
-    @Inject(method = "onLivingDrops", at = @At("HEAD"), cancellable = true)
-    public void onLivingDrops(LivingDropsEvent event, CallbackInfo ci) {
-        ci.cancel();
+    @Overwrite
+    public void onLivingDrops(LivingDropsEvent event) {
+
     }
 }
