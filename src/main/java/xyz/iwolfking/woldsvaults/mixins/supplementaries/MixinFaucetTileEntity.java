b44b51c42b64f71ae67284835669b638f5ff8ae6
Kyle Robertson <me@iwolfking.xyz>
2024-09-04 20:39:52 -0500

    Supplementaries patch

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/supplementaries/MixinFaucetTileEntity.java
@@ -0,0 +1,42 @@
+package xyz.iwolfking.woldsvaults.mixins.supplementaries;
+
+import me.desht.modularrouters.block.ModularRouterBlock;
+import me.desht.modularrouters.block.tile.ModularRouterBlockEntity;
+import net.mehvahdjukaar.supplementaries.common.block.blocks.FaucetBlock;
+import net.mehvahdjukaar.supplementaries.common.block.tiles.FaucetBlockTile;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(value = FaucetBlockTile.class, remap = false)
+public abstract class MixinFaucetTileEntity {
+    private static final List<Item> ROUTER_ALLOWED_FLUIDS = List.of(Items.WATER_BUCKET, Items.LAVA_BUCKET);
+    /**
+     * @author iwolfking
+     * @reason Patch Supplementaries bug with Modular Routers (excluding water and lava)
+     */
+    @Inject(method = "tryExtract", at = @At("HEAD"), cancellable = true)
+    private void tryExtract(Level level, BlockPos pos, BlockState state, boolean doTransfer, CallbackInfoReturnable<Boolean> cir) {
+        Direction dir = (Direction) state.getValue(FaucetBlock.FACING);
+        BlockPos behind = pos.relative(dir.getOpposite());
+        BlockState backState = level.getBlockState(behind);
+        Block backBlock = backState.getBlock();
+        if(backBlock instanceof ModularRouterBlock) {
+            if(level.getBlockEntity(behind) instanceof ModularRouterBlockEntity blockEntity) {
+                if(!ROUTER_ALLOWED_FLUIDS.contains(blockEntity.getBuffer().getStackInSlot(0).getItem())) {
+                    cir.setReturnValue(false);
+                }
+            }
+        }
+    }
+}
