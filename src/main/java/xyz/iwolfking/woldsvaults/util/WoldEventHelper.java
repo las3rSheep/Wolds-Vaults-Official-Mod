647616d4e40954bd6f2b0da5ac7f07e2fb9572a2
Kyle Robertson <me@iwolfking.xyz>
2024-09-14 18:56:51 -0500

    Fix Thorns proccing reaving

--- src/main/java/xyz/iwolfking/woldsvaults/util/WoldEventHelper.java
+++ src/main/java/xyz/iwolfking/woldsvaults/util/WoldEventHelper.java
@@ -4,6 +4,6 @@ import iskallia.vault.event.ActiveFlags;
 
 public class WoldEventHelper {
     public static boolean isNormalAttack() {
-        return !ActiveFlags.IS_AOE_ATTACKING.isSet() && !ActiveFlags.IS_TOTEM_ATTACKING.isSet() && !ActiveFlags.IS_CHARMED_ATTACKING.isSet() && !ActiveFlags.IS_DOT_ATTACKING.isSet() && !ActiveFlags.IS_REFLECT_ATTACKING.isSet() && !ActiveFlags.IS_EFFECT_ATTACKING.isSet() && !ActiveFlags.IS_AP_ATTACKING.isSet();
+        return !ActiveFlags.IS_AOE_ATTACKING.isSet() && !ActiveFlags.IS_TOTEM_ATTACKING.isSet() && !ActiveFlags.IS_CHARMED_ATTACKING.isSet() && !ActiveFlags.IS_DOT_ATTACKING.isSet() && !ActiveFlags.IS_REFLECT_ATTACKING.isSet() && !ActiveFlags.IS_EFFECT_ATTACKING.isSet() && !ActiveFlags.IS_AP_ATTACKING.isSet() && !ActiveFlags.IS_THORNS_REFLECTING.isSet();
     }
 }
