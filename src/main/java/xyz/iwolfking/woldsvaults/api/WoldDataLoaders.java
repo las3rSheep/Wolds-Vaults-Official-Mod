c28a06e35250510d87544dd0fbf37806c6a41aff
Kyle Robertson <me@iwolfking.xyz>
2024-10-10 10:47:43 -0500

    Add data loaders for Wold's Boxes

---
+++ src/main/java/xyz/iwolfking/woldsvaults/api/WoldDataLoaders.java
@@ -0,0 +1,52 @@
+package xyz.iwolfking.woldsvaults.api;
+
+import xyz.iwolfking.vhapi.api.lib.config.loaders.box.WeightedProductEntryConfigLoader;
+import xyz.iwolfking.woldsvaults.init.ModConfigs;
+
+public class WoldDataLoaders {
+
+    public static final WeightedProductEntryConfigLoader AUGMENT_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader INSCRIPTION_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader OMEGA_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader VAULTAR_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader CATALYST_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader GEM_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader SUPPLY_BOX_LOADER;
+    public static final WeightedProductEntryConfigLoader ENIGMA_EGG_LOADER;
+
+    static {
+        AUGMENT_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.AUGMENT_BOX.POOL;
+        }, "augment_box");
+
+        INSCRIPTION_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.INSCRIPTION_BOX.POOL;
+        }, "inscription_box");
+
+        OMEGA_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.OMEGA_BOX.POOL;
+        }, "omega_box");
+
+        VAULTAR_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.VAULTAR_BOX.POOL;
+        }, "vaultar_box");
+
+        CATALYST_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.VAULTAR_BOX.POOL;
+        }, "catalyst_box");
+
+        GEM_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.GEM_BOX.POOL;
+        }, "gem_box");
+
+        SUPPLY_BOX_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.SUPPLY_BOX.POOL;
+        }, "supply_box");
+
+        ENIGMA_EGG_LOADER = new WeightedProductEntryConfigLoader("vhapi", () -> {
+            return ModConfigs.ENIGMA_EGG.POOL;
+        }, "enigma_egg");
+
+    }
+
+}
