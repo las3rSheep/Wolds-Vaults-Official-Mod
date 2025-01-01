6e88e95a002eac6e27c5a01c27d3b682ec4197c6
iwolfking <thegreatiwk@gmail.com>
2025-01-01 08:44:57 -0600

    Add custom models for kaleidoscope, manabloom, treasure jewel

--- src/main/java/xyz/iwolfking/woldsvaults/models/Jewels.java
+++ src/main/java/xyz/iwolfking/woldsvaults/models/Jewels.java
@@ -8,8 +8,14 @@ import iskallia.vault.dynamodel.registry.DynamicModelRegistry;
 public class Jewels {
     public static final DynamicModelRegistry<PlainItemModel> REGISTRY = new DynamicModelRegistry<>();
     public static final PlainItemModel STELLA;
+    public static final PlainItemModel TREASURE;
+    public static final PlainItemModel MANABLOOM;
+    public static final PlainItemModel KALEIDOSCOPE;
 
     static {
         STELLA = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/stella"), "Eternal Stella")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        TREASURE = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/treasure"), "Treasured Jewel")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        MANABLOOM = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/manabloom"), "Manabloom")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        KALEIDOSCOPE = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/kaleidoscope"), "Kaleidoscope")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
     }
 }
