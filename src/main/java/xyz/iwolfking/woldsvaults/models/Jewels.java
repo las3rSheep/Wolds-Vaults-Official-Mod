31af1079afcaa3629cfaca8a4e470f90cf21f830
iwolfking <thegreatiwk@gmail.com>
2025-01-01 08:57:39 -0600

    Add Pax jewel texture

--- src/main/java/xyz/iwolfking/woldsvaults/models/Jewels.java
+++ src/main/java/xyz/iwolfking/woldsvaults/models/Jewels.java
@@ -11,11 +11,13 @@ public class Jewels {
     public static final PlainItemModel TREASURE;
     public static final PlainItemModel MANABLOOM;
     public static final PlainItemModel KALEIDOSCOPE;
+    public static final PlainItemModel PAX;
 
     static {
         STELLA = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/stella"), "Eternal Stella")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
         TREASURE = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/treasure"), "Treasured Jewel")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
         MANABLOOM = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/manabloom"), "Manabloom")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
         KALEIDOSCOPE = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/kaleidoscope"), "Kaleidoscope")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        PAX = REGISTRY.register(new PlainItemModel(VaultMod.id("gear/jewel/pax"), "Pax")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
     }
 }
