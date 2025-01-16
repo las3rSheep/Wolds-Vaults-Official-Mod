1c0e128e1bce65b69f8e6cf29e9dfd276b281dc0
radimous <jurda92@gmail.com>
2025-01-17 00:55:54 +0100

    add unusual item overlay

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/enum_extension/MixinAffixCategoryEnum.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/enum_extension/MixinAffixCategoryEnum.java
@@ -1,7 +1,9 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom.enum_extension;
 
+import iskallia.vault.VaultMod;
 import iskallia.vault.gear.attribute.VaultGearModifier;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -22,18 +24,18 @@ public class MixinAffixCategoryEnum {
     @SuppressWarnings("target")
     private static VaultGearModifier.AffixCategory[] $VALUES;
 
-    private static final VaultGearModifier.AffixCategory UNUSUAL = enumExpansion$addVariant("UNUSUAL", "Unusual", CustomModifierCategoryTooltip::modifyUnusualTooltip);
+    private static final VaultGearModifier.AffixCategory UNUSUAL = enumExpansion$addVariant("UNUSUAL", "Unusual", CustomModifierCategoryTooltip::modifyUnusualTooltip, VaultMod.id("textures/item/gear/unusual_overlay.png"));
 
 
     @Invoker("<init>")
-    public static VaultGearModifier.AffixCategory enumExpansion$invokeInit(String internalName, int internalId, String descriptor, Function<MutableComponent, MutableComponent> modifierFormatter) {
+    public static VaultGearModifier.AffixCategory enumExpansion$invokeInit(String internalName, int internalId, String descriptor, Function<MutableComponent, MutableComponent> modifierFormatter, ResourceLocation overlayIcon) {
         throw new AssertionError();
     }
 
     @Unique
-    private static VaultGearModifier.AffixCategory enumExpansion$addVariant(String internalName, String descriptor, Function<MutableComponent, MutableComponent> modifierFormatter) {
+    private static VaultGearModifier.AffixCategory enumExpansion$addVariant(String internalName, String descriptor, Function<MutableComponent, MutableComponent> modifierFormatter, ResourceLocation overlayIcon) {
         ArrayList<VaultGearModifier.AffixCategory> variants = new ArrayList<>(Arrays.asList(MixinAffixCategoryEnum.$VALUES));
-        VaultGearModifier.AffixCategory type = enumExpansion$invokeInit(internalName, variants.get(variants.size() - 1).ordinal() + 1, descriptor, modifierFormatter);
+        VaultGearModifier.AffixCategory type = enumExpansion$invokeInit(internalName, variants.get(variants.size() - 1).ordinal() + 1, descriptor, modifierFormatter, overlayIcon);
         variants.add(type);
         MixinAffixCategoryEnum.$VALUES = variants.toArray(new VaultGearModifier.AffixCategory[0]);
         return type;
