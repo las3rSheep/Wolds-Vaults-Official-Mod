20a354a3f7de98b4bfcac7763ab7e5223c7330c7
iwolfking <thegreatiwk@gmail.com>
2025-01-19 11:43:30 -0600

    Weapon Type can display as a Base Attribute

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinVaultGearTooltipItem.java
@@ -0,0 +1,41 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
+
+import iskallia.vault.gear.attribute.VaultGearAttributeInstance;
+import iskallia.vault.gear.attribute.VaultGearModifier;
+import iskallia.vault.gear.data.VaultGearData;
+import iskallia.vault.gear.tooltip.VaultGearTooltipItem;
+import iskallia.vault.init.ModGearAttributes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Mixin(value = VaultGearTooltipItem.class, remap = false)
+public interface MixinVaultGearTooltipItem{
+    /**
+     * @author iwolfking
+     * @reason Add Weapon type to display as a base attribute
+     */
+    @Overwrite
+    default void addTooltipImportantBaseAttributes(VaultGearData data, ItemStack stack, List<Component> tooltip, boolean displayDetail) {
+        int txtLength = tooltip.size();
+        data.getAttributes(ModGearAttributes.DURABILITY)
+                .filter(inst -> inst.getValue() > 0)
+                .forEach(attr -> attr.getDisplay(data, VaultGearModifier.AffixType.IMPLICIT, stack, displayDetail).ifPresent(tooltip::add));
+        data.getAttributes(ModGearAttributes.SOULBOUND)
+                .filter(VaultGearAttributeInstance::getValue)
+                .forEach(attr -> attr.getDisplay(data, VaultGearModifier.AffixType.IMPLICIT, stack, displayDetail).ifPresent(tooltip::add));
+        data.getAttributes(xyz.iwolfking.woldsvaults.init.ModGearAttributes.WEAPON_TYPE)
+                .filter(Objects::nonNull)
+                .forEach(attr -> attr.getDisplay(data, VaultGearModifier.AffixType.IMPLICIT, stack, displayDetail).ifPresent(tooltip::add));
+        if (tooltip.size() > txtLength && displayDetail) {
+            tooltip.add(TextComponent.EMPTY);
+        }
+
+    }
+}
