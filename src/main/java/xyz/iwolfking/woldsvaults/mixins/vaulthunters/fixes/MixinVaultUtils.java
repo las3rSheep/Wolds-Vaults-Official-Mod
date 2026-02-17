7574c594265ee2aa1a1e487e80d9590ee1904d37
radimous <jurda92@gmail.com>
2026-02-17 10:15:05 +0100

    revert infinite layout patch, globe/compass will no longer work outside of the first 6 rooms

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinVaultUtils.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinVaultUtils.java
@@ -3,8 +3,6 @@ package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.VaultUtils;
 import iskallia.vault.core.vault.objective.Objective;
-import iskallia.vault.core.world.generator.layout.ClassicInfiniteLayout;
-import iskallia.vault.core.world.generator.layout.VaultGridLayout;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,15 +20,8 @@ public abstract class MixinVaultUtils {
 
     @Inject(method = "isBrazierVault", at = @At("HEAD"), cancellable = true)
     private static void isHauntedBraziersVault(Vault vault, CallbackInfoReturnable<Boolean> cir) {
-        if(hasObjective(vault, HauntedBraziersObjective.class)) {
+        if (hasObjective(vault, HauntedBraziersObjective.class)) {
             cir.setReturnValue(true);
         }
-    }
-
-    @Inject(method = "getVaultRadius", at = @At("HEAD"), cancellable = true)
-    private static void fixInfiniteRadius(VaultGridLayout layout, Vault vault, CallbackInfoReturnable<Integer> cir){
-       if (layout instanceof ClassicInfiniteLayout) {
-           cir.setReturnValue(100);
-       }
     }
 }
