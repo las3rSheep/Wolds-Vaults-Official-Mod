d32ae2b5fab1f56d944ff2bc4540cb4c980598d0
iwolfking <thegreatiwk@gmail.com>
2026-03-30 16:37:42 -0500

    Also prevent placing Crystal Workbench with Effortless Building (Fixes #362)

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/effortlessbuilding/MixinEventHandler.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/effortlessbuilding/MixinEventHandler.java
@@ -1,5 +1,6 @@
 package xyz.iwolfking.woldsvaults.mixins.effortlessbuilding;
 
+import iskallia.vault.block.CrystalWorkbenchBlock;
 import iskallia.vault.block.base.InventoryRetainerBlock;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
@@ -19,7 +20,7 @@ public abstract class MixinEventHandler {
 
     @Inject(method = "onBlockPlaced", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), cancellable = true, remap = true)
     private static void preventPlacingRetainerBlocks(BlockEvent.EntityPlaceEvent event, CallbackInfo ci) {
-        if(event.getPlacedBlock().getBlock() instanceof InventoryRetainerBlock<?>) {
+        if(event.getPlacedBlock().getBlock() instanceof InventoryRetainerBlock<?> || event.getPlacedBlock().getBlock() instanceof CrystalWorkbenchBlock) {
             ci.cancel();
             return;
         }
