148d567c925f92ca078f6535e1d6ae1003881649
iwolfking <thegreatiwk@gmail.com>
2026-04-05 07:43:49 -0500

    Fix Brutal Bosses objective rendering

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinObeliskObjective.java
@@ -0,0 +1,15 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes;
+
+import com.mojang.blaze3d.platform.Window;
+import iskallia.vault.core.vault.objective.ObeliskObjective;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = ObeliskObjective.class, remap = false)
+public class MixinObeliskObjective {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getGuiScaledWidth()I"))
+    private int fixObjectiveRendering(Window instance) {
+        return 0;
+    }
+}
