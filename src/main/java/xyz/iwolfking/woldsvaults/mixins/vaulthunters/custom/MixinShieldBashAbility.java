55ba5a6edc3e3f3bef42c66007af054dd3f18bff
radimous <jurda92@gmail.com>
2026-04-14 19:43:41 +0200

    proper remaps

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinShieldBashAbility.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinShieldBashAbility.java
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = ShieldBashAbility.class)
+@Mixin(value = ShieldBashAbility.class, remap = false)
 public class MixinShieldBashAbility {
-    @Redirect(method = "lambda$doAction$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
+    @Redirect(method = "lambda$doAction$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", remap = true))
     private boolean doThornsDamage(LivingEntity instance, DamageSource pSource, float pAmount, @Local(name = "player") ServerPlayer player) {
         float thornsDamage = ThornsHelper.getAdditionalThornsFlatDamage(player);
         return instance.hurt(pSource, pAmount + thornsDamage);
