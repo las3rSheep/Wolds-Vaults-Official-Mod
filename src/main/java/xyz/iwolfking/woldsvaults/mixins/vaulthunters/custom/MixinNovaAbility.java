ab282872726c78ef4ccdab87dfb254bad5cf1d8c
iwolfking <thegreatiwk@gmail.com>
2026-04-21 11:57:21 -0500

    Adjust damage falloff per level for nova instead

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinNovaAbility.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinNovaAbility.java
@@ -1,17 +1,28 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import iskallia.vault.skill.ability.effect.NovaAbility;
+import iskallia.vault.skill.base.SkillContext;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = NovaAbility.class, remap = false)
-public class MixinNovaAbility {
-    @Inject(method = "getDamageModifier", at = @At("TAIL"), cancellable = true)
-    private void reduceDamageFallOff(float radius, float dist, CallbackInfoReturnable<Float> cir) {
-        if(cir.getReturnValue() <= 0.5F) {
-            cir.setReturnValue(0.5F);
-        }
+public abstract class MixinNovaAbility {
+
+    @Shadow
+    protected abstract float getDamageModifier(float radius, float dist);
+
+    @Redirect(method = "lambda$doAction$4", at = @At(value = "INVOKE", target = "Liskallia/vault/skill/ability/effect/NovaAbility;getDamageModifier(FF)F"))
+    private float callAdjustedDamageModifier(NovaAbility instance, float radius, float dist, @Local(argsOnly = true) SkillContext skillContext) {
+        return woldsVaults$getLevelScaledDamageFallOff(this.getDamageModifier(radius, dist), skillContext.getLevel());
+    }
+
+    @Unique
+    private float woldsVaults$getLevelScaledDamageFallOff(float originalFalloff, int abilityLevel) {
+        float maxFallOffPerLevel = 0.2F + (abilityLevel * 0.025F);
+        return Math.max(originalFalloff, maxFallOffPerLevel);
     }
 }
