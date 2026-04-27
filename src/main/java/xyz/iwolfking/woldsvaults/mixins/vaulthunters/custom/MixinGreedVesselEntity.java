2b5fc484d2ae235794930070b8a37188cf8ef8b3
JGrade <jgrade942@gmail.com>
2026-04-27 02:22:59 -0400

    Initial Vessel Effect Adjust

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinGreedVesselEntity.java
@@ -0,0 +1,40 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import iskallia.vault.init.ModEffects;
+import iskallia.vault.entity.boss.TheVesselEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mixin(value = TheVesselEntity.class, remap = false)
+public class MixinGreedVesselEntity {
+    @Unique
+    private static final Set<MobEffect> woldsvaults$possibleVesselEffects = new HashSet<>();
+    static {
+        woldsvaults$possibleVesselEffects.add(MobEffects.POISON);
+        woldsvaults$possibleVesselEffects.add(MobEffects.MOVEMENT_SLOWDOWN);
+        woldsvaults$possibleVesselEffects.add(MobEffects.WEAKNESS);
+        woldsvaults$possibleVesselEffects.add(ModEffects.SMITE);
+        woldsvaults$possibleVesselEffects.add(ModEffects.TOTEM_MOB_DAMAGE);
+        woldsvaults$possibleVesselEffects.add(ModEffects.BLEED);
+        woldsvaults$possibleVesselEffects.add(ModEffects.NOVA_DOT);
+        woldsvaults$possibleVesselEffects.add(ModEffects.CHILLED);
+        woldsvaults$possibleVesselEffects.add(xyz.iwolfking.woldsvaults.init.ModEffects.ECHOING);
+        woldsvaults$possibleVesselEffects.add(xyz.iwolfking.woldsvaults.init.ModEffects.REAVING);
+    }
+    @SuppressWarnings("unused")
+    @Inject(method = "m_7301_", at = @At("Head"), cancellable = true)
+    
+    private void woldsvaults$allowedVesselEffects(MobEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
+        if (woldsvaults$possibleVesselEffects.contains(effect.getEffect())) {
+            cir.setReturnValue(true);
+        }
+    }
+}
