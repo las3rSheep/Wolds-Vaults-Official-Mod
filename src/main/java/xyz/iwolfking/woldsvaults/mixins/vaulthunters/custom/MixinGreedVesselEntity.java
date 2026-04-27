f42321270e93563271129632de2eb097e545f524
JGrade <jgrade942@gmail.com>
2026-04-27 12:35:24 -0400

    Readd HashSet handling for allow effects

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinGreedVesselEntity.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinGreedVesselEntity.java
@@ -12,26 +12,30 @@ import iskallia.vault.entity.boss.TheVesselEntity;
 import java.util.HashSet;
 import java.util.Set;
 
+
+
 @Mixin(value = TheVesselEntity.class, remap = false)
 public class MixinGreedVesselEntity {
+
+    @Unique
+    private static final Set<MobEffect> woldsvaults$possibleVesselEffects = new HashSet<>();
+    static {
+        woldsvaults$possibleVesselEffects.add(MobEffects.POISON);
+        woldsvaults$possibleVesselEffects.add(MobEffects.MOVEMENT_SLOWDOWN);
+        woldsvaults$possibleVesselEffects.add(MobEffects.WEAKNESS);
+        woldsvaults$possibleVesselEffects.add(ModEffects.SMITE);
+        woldsvaults$possibleVesselEffects.add(ModEffects.TOTEM_MOB_DAMAGE);
+        woldsvaults$possibleVesselEffects.add(ModEffects.BLEED);
+        woldsvaults$possibleVesselEffects.add(ModEffects.NOVA_DOT);
+        woldsvaults$possibleVesselEffects.add(ModEffects.CHILLED);
+        woldsvaults$possibleVesselEffects.add(xyz.iwolfking.woldsvaults.init.ModEffects.ECHOING);
+        woldsvaults$possibleVesselEffects.add(xyz.iwolfking.woldsvaults.init.ModEffects.REAVING);
+    }
+
     @Inject(method = "canBeAffected" , at = @At("TAIL"), cancellable = true, remap = true)
     private void allowedVesselEffects(MobEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
-        if (effect.getEffect() == MobEffects.POISON ||
-                effect.getEffect() == MobEffects.MOVEMENT_SLOWDOWN ||
-                effect.getEffect() == MobEffects.WEAKNESS ||
-                effect.getEffect() == ModEffects.SMITE ||
-                effect.getEffect() == ModEffects.TOTEM_MOB_DAMAGE ||
-                effect.getEffect() == ModEffects.BLEED ||
-                effect.getEffect() == ModEffects.NOVA_DOT ||
-                effect.getEffect() == ModEffects.CHILLED ||
-                effect.getEffect() == xyz.iwolfking.woldsvaults.init.ModEffects.ECHOING ||
-                effect.getEffect() == xyz.iwolfking.woldsvaults.init.ModEffects.REAVING)
-        {
+        if (woldsvaults$possibleVesselEffects.contains(effect.getEffect())) {
             cir.setReturnValue(true);
         }
-
-        //if (woldsvaults$possibleVesselEffects.contains(effect.getEffect())) {
-        //    cir.setReturnValue(true);
-        //}
     }
 }
