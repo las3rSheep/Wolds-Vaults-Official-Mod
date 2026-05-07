9160ccd64d9c8f6fc40258dda26797cc24b422e9
iwolfking <thegreatiwk@gmail.com>
2026-05-07 06:03:06 -0500

    Implode and Life Tap also scale damage fall-off with level like Nova

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinLifeTapAbility.java
@@ -0,0 +1,29 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import iskallia.vault.skill.ability.effect.ImplodeAbility;
+import iskallia.vault.skill.ability.effect.LifeTapAbility;
+import iskallia.vault.skill.base.SkillContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = LifeTapAbility.class, remap = false)
+public abstract class MixinLifeTapAbility {
+
+    @Shadow
+    protected abstract float getDamageModifier(float radius, float dist);
+
+    @Redirect(method = "lambda$doAction$3", at = @At(value = "INVOKE", target = "Liskallia/vault/skill/ability/effect/LifeTapAbility;getDamageModifier(FF)F"))
+    private float callAdjustedDamageModifier(LifeTapAbility instance, float radius, float dist, @Local(argsOnly = true) SkillContext skillContext) {
+        return woldsVaults$getLevelScaledDamageFallOff(this.getDamageModifier(radius, dist), skillContext.getLevel());
+    }
+
+    @Unique
+    private float woldsVaults$getLevelScaledDamageFallOff(float originalFalloff, int abilityLevel) {
+        float maxFallOffPerLevel = 0.2F + (abilityLevel * 0.025F);
+        return Math.max(originalFalloff, maxFallOffPerLevel);
+    }
+}
