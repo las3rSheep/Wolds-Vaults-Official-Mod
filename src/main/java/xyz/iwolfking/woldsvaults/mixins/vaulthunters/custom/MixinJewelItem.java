5a64697ee553531884b19e2b851879914cd69e02
radimous <jurda92@gmail.com>
2026-05-18 11:22:09 +0200

    fix jewel name color

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinJewelItem.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinJewelItem.java
@@ -1,11 +1,15 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
+import iskallia.vault.client.util.ClientScheduler;
 import iskallia.vault.core.random.RandomSource;
 import iskallia.vault.dynamodel.DynamicModel;
 import iskallia.vault.dynamodel.DynamicModelItem;
+import iskallia.vault.gear.VaultGearState;
+import iskallia.vault.gear.data.GearDataCache;
 import iskallia.vault.gear.item.VaultGearItem;
 import iskallia.vault.item.core.DataInitializationItem;
 import iskallia.vault.item.core.DataTransferItem;
+import iskallia.vault.item.render.ColorBlender;
 import iskallia.vault.item.tool.JewelItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -17,6 +21,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.iwolfking.woldsvaults.models.Jewels;
 
@@ -62,5 +67,17 @@ public abstract class MixinJewelItem extends Item implements VaultGearItem, Data
     @Inject(method = "getColor", at = @At("HEAD"), cancellable = true)
     private static void noColorIfModel(ItemStack stack, CallbackInfoReturnable<Integer> cir){
         ((DynamicModelItem) stack.getItem()).getDynamicModelId(stack).ifPresent(modelId -> cir.setReturnValue(0xFFFFFF));
+    }
+
+    @Redirect(method = "getName", at = @At(value = "INVOKE", target = "Liskallia/vault/item/tool/JewelItem;getColor(Lnet/minecraft/world/item/ItemStack;)I"))
+    private int keepColoredName(ItemStack stack){
+        GearDataCache clientCache = GearDataCache.of(stack);
+        if (clientCache.getState() == VaultGearState.UNIDENTIFIED) {
+            return -12632257;
+        }
+        ColorBlender blender = new ColorBlender(1.0F);
+        Optional.ofNullable(clientCache.getJewelColorComponents()).ifPresent((colors) -> colors.forEach((color) -> blender.add(color, 60.0F)));
+        float time = ClientScheduler.INSTANCE.getTick();
+        return blender.getColor(time);
     }
 }
