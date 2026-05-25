f0e46931a2c8a9e4faf2f852c933877ff5c05f3f
iwolfking <thegreatiwk@gmail.com>
2026-05-25 07:59:25 -0500

    RFTools Crafter won't allow applying locked recipes either

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/rftoolsutility/MixinCrafterBaseTE.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/rftoolsutility/MixinCrafterBaseTE.java
@@ -1,15 +1,18 @@
 package xyz.iwolfking.woldsvaults.mixins.rftoolsutility;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import iskallia.vault.research.Restrictions;
 import iskallia.vault.world.data.PlayerResearchesData;
 import mcjty.lib.tileentity.TickingTileEntity;
 import mcjty.rftoolsbase.api.compat.JEIRecipeAcceptor;
 import mcjty.rftoolsutility.modules.crafter.blocks.CrafterBaseTE;
+import mcjty.rftoolsutility.modules.crafter.data.CraftingRecipe;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,6 +20,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.iwolfking.vhapi.api.util.MessageUtils;
 
 @Restriction(
         require = {
@@ -27,6 +31,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinCrafterBaseTE extends TickingTileEntity implements JEIRecipeAcceptor {
     public MixinCrafterBaseTE(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    @WrapWithCondition(method = "applyRecipe", at = @At(value = "INVOKE", target = "Lmcjty/rftoolsutility/modules/crafter/data/CraftingRecipe;setRecipe([Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)V"))
+    private boolean cancelApplyingRecipe(CraftingRecipe instance, ItemStack[] items, ItemStack result) {
+        return getOwnerUUID() != null && PlayerResearchesData.get((ServerLevel) this.level).getResearches(getOwnerUUID()).restrictedBy(result, Restrictions.Type.CRAFTABILITY) == null;
     }
 
     @Inject(method = "craftOneItem", at = @At(value = "INVOKE", target = "Lmcjty/lib/container/UndoableItemHandler;<init>(Lnet/minecraftforge/items/IItemHandlerModifiable;)V"), cancellable = true)
