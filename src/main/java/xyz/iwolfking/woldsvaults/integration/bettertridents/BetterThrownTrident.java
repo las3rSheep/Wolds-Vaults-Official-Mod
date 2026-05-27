73ed9710a760714ba808d4e0099e3ae393ebfbf2
aida <152106387+las3rSheep@users.noreply.github.com>
2026-05-26 23:37:18 -0400

    make vaulttridents return from the void

--- src/main/java/xyz/iwolfking/woldsvaults/integration/bettertridents/BetterThrownTrident.java
+++ src/main/java/xyz/iwolfking/woldsvaults/integration/bettertridents/BetterThrownTrident.java
@@ -3,11 +3,17 @@ package xyz.iwolfking.woldsvaults.integration.bettertridents;
 import fuzs.bettertridents.BetterTridents;
 import fuzs.bettertridents.config.ServerConfig;
 import fuzs.bettertridents.init.ModRegistry;
+import iskallia.vault.gear.attribute.type.VaultGearAttributeTypeMerger;
+import iskallia.vault.gear.data.VaultGearData;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import static xyz.iwolfking.woldsvaults.init.ModGearAttributes.TRIDENT_LOYALTY;
 
 public class BetterThrownTrident extends ThrownTrident {
     public BetterThrownTrident(Level level, Player player, ItemStack stack) {
@@ -24,6 +30,30 @@ public class BetterThrownTrident extends ThrownTrident {
                     capability.setSlot(player.getInventory().selected);
                 }
             });
+        }
+    }
+
+    @Override
+    protected void outOfWorld() {
+        if (!BetterTridents.CONFIG.get(ServerConfig.class).returnTridentFromVoid) {
+            super.outOfWorld();
+            return;
+        }
+        VaultGearData data = VaultGearData.read(this.getPickupItem());
+        if (data.get(TRIDENT_LOYALTY, VaultGearAttributeTypeMerger.intSum()) >= 1 && this.canReturn()) {
+            this.setNoPhysics(true);
+            this.playerTouch((Player) this.getOwner());
+            return;
+        }
+        super.outOfWorld();
+    }
+
+    private boolean canReturn() {
+        Entity owner = this.getOwner();
+        if (owner != null && owner.isAlive()) {
+            return !(owner instanceof ServerPlayer) || !owner.isSpectator();
+        } else {
+            return false;
         }
     }
 }
